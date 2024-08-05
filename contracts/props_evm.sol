81519e49397ff0c8c3236dfafc4877a62487f640
harispericorn <hari.k@spericorn.com>
2024-08-05 19:00:15 +0530

    PROPS implemnted

--- contracts/props_evm.sol
+++ contracts/props_evm.sol
@@ -6,49 +6,88 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+error PROPS__InvalidAddress();
+error PROPS__MintCapReached(uint256 limit);
+error PROPS__AddressNotMultiSign();
+error PROPS__AmountTrancheCapReached();
+error PROPS__BurnAmountNotAvailable();
 
 contract PROPS is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     uint256 public immutable MINT_CAP;
+    string public _iconURI;
     uint256 current_supply = 0;
+    uint256 mint_tranche_cap = 0;
+    uint256 burn_tranche_cap = 0;
 
     constructor(
         uint256 mint_cap,
-        address reciever,
-        uint256 amount
+        uint256 mint_tranche,
+        uint256 burn_tranche,
+        string memory icon_uri
     )
         ERC20("PROPS", "PROPS")
     {
         MINT_CAP = mint_cap;
+        _iconURI = icon_uri;
+        mint_tranche_cap = mint_tranche;
+        burn_tranche_cap = burn_tranche;
         _setupRole(ADMIN_ROLE, msg.sender);
-        _mint(reciever, amount * (10 ** decimals()));
     }
 
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    modifier isValidAddress(address to){
+        if(to == address(0)){
+            revert PROPS__InvalidAddress(); 
+        }
+        _;
+    }
+
+    modifier isMultisignAddress(address to){
+        if(to.code.length == 0){
+            revert PROPS__AddressNotMultiSign(); 
+        }
+        _;
+    }
+
+    modifier checkTrancheLimit(uint256 amount, uint256 limit_cap){
+        if(amount > limit_cap){
+            revert PROPS__AmountTrancheCapReached(); 
+        }
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) checkTrancheLimit(amount, mint_tranche_cap) {
         current_supply += amount;
-        require(current_supply <= MINT_CAP, "Maximum number of tokens already minted");
+        if(current_supply > MINT_CAP){
+            revert PROPS__MintCapReached(MINT_CAP);
+        }
         _mint(to, amount);
     }
 
-    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
+    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) checkTrancheLimit(amount, burn_tranche_cap) {
+        if(current_supply < amount){
+            revert PROPS__BurnAmountNotAvailable(); 
+        }
         current_supply -= amount;
         _burn(to, amount);
     }
 
-    function decimals() override public view returns (uint8) {
+    function decimals() public override view virtual returns (uint8) {
         return 8;
     }
-    
-    function setupMinter(address minter, bool enabled) external onlyRole(ADMIN_ROLE) {
-        require(minter != address(0), "!minter");
+
+    function setupAdmin(address admin) external onlyRole(ADMIN_ROLE) isValidAddress(admin) isMultisignAddress(admin) {
+        _setupRole(ADMIN_ROLE, admin);   
+    }
+
+    function setupMinter(address minter, bool enabled) external onlyRole(ADMIN_ROLE) isValidAddress(minter) isMultisignAddress(minter) {
         if (enabled) _setupRole(MINTER_ROLE, minter);
         else _revokeRole(MINTER_ROLE, minter);   
     }
 
-    function setupBurner(address burner, bool enabled) external onlyRole(ADMIN_ROLE) {
-        require(burner != address(0), "!burner");
+    function setupBurner(address burner, bool enabled) external onlyRole(ADMIN_ROLE) isValidAddress(burner) isMultisignAddress(burner) {
         if (enabled) _setupRole(BURNER_ROLE, burner);
         else _revokeRole(BURNER_ROLE, burner);   
     }  
